13b5826861af7efa079b566422f0a402dc92fe29
Keerthi <sriram960326@gmail.com>
2022-02-04 01:03:02 +0530

    v0.1: app opens only when button is pressed in popup.html. This ensures that script is not injected in every page

--- popup.js
+++ popup.js
@@ -1,6 +1,33 @@
-'use strict';
+
+// following code injects the following js and css code when button is clicked
+
+var jsFiles = ["ext_libs/jquery.min.js","ext_libs/jquery-ui.min.js","ext_libs/jquery.cookie.min.js","ext_libs/popper.js","ext_libs/pell.min.js","ext_libs/notify.min.js","webresearcher/webresearcher.js","ext_libs/bootstrap.min.js"];
+var cssFiles = ["ext_libs/pell.css","ext_libs/jquery-ui.min.css","ext_libs/bootstrap.min.css"];
 
 
-	document.getElementById("settings").addEventListener('click', () =>
-		browser.runtime.openOptionsPage().then(_ => window.close())
-	);
+document.addEventListener('DOMContentLoaded', function() {
+    var link = document.getElementById('go');
+    link.addEventListener('click', function() {
+
+
+			for(var i=0;i<jsFiles.length;i++){
+				const executing = browser.tabs.executeScript({
+				file: jsFiles[i]
+		  });
+		 }
+
+		 for(var i=0;i<cssFiles.length;i++){
+			 const executing = browser.tabs.insertCSS({
+			 file: cssFiles[i]
+		 });
+		}
+
+
+
+
+
+
+
+
+    });
+});
