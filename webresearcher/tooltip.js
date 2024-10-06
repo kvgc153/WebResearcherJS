d409ed6e8f78077c6ba4ae42c43c7e1130a7fcd2
Keerthi <kvch153@gmail.com>
2024-10-06 00:23:32 -0700

    fixing minor css + update prompt

--- webresearcher/tooltip.js
+++ webresearcher/tooltip.js
@@ -14,6 +14,10 @@ function makeTooltip(){
   
     const button = document.createElement('button');
     button.innerText = 'Summarize';
+    button.style.zIndex = '1000';
+    button.style.backgroundColor = 'blue';
+    button.style.color = 'white';
+    button.style.position = 'fixed';
     button.id = "summarizeWBJS";  
     button.className = 'btn';
     tooltip.appendChild(button);
@@ -40,9 +44,9 @@ function makeTooltip(){
         tooltip.style.top = `${rect.bottom + window.scrollY}px`;
         tooltip.style.display = 'block';
     } 
-    // else {
-    //     tooltip.style.display = 'none';
-    // }
+    else {
+        tooltip.style.display = 'none';
+    }
   }
 
   var userSeletedText = "";
@@ -67,7 +71,7 @@ function makeTooltip(){
         "messages": [
             { 
               "role": "user",
-              "content": "Summarize this in two sentences and give me tags in hashtag format to remember them later: " + context
+              "content": "Summarize the following in two sentences and give me tags in hashtag format to remember them later. Print only the summary and tags. Text: " + context
             }
         ]
   
