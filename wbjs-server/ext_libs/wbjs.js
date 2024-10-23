1f2a62c84f1155b30308dc57a020093e3852e40e
Keerthi <kvch153@gmail.com>
2024-10-23 15:26:42 -0700

    upgrading the notes search view

--- wbjs-server/ext_libs/wbjs.js
+++ wbjs-server/ext_libs/wbjs.js
@@ -14,105 +14,109 @@ let colorsNotes = [
 
 ];
 
-function displayNotes(data){
+function displayNotes(data) {
     var allData = data;
     var allDataKeys = Object.keys(allData);
+
+    for (let i = 0; i < allDataKeys.length; i++) {
+        var edjsData = JSON.parse(allData[allDataKeys[i]]);
+        var foo_loaded_keys = Object.keys(edjsData['JSON']);
     
-    // Loop through all the notes 
-    for(let i=0;i<allDataKeys.length;i++){
-
-        var edjsData   = JSON.parse(allData[allDataKeys[i]]);
-        var foo_loaded_keys   = Object.keys(edjsData['JSON']);
-        var colorChosen = colorsNotes[Math.floor(Math.random()*colorsNotes.length)];
-        
-        var noteContent = document.createElement('div');
-        noteContent.className = 'note-content';
-        noteContent.id = 'note-content' + i;
-
-
+        var noteContentWrapper = document.createElement('div');
+        noteContentWrapper.className = 'note-content';
+    
         var noteContent1 = document.createElement('div');
-        // var editedTime = new Date(edjsData['JSON'][foo_loaded_keys[0]]['time']).toString();
-        noteContent1.innerHTML = "<h1>" + "<a href='" + 'http://' + allDataKeys[i] + "'>" + edjsData['TITLE'] + "</a></h1>" +  edjsData['TAGS'] + "<br>";
-
-
-
-        if(foo_loaded_keys.length>0){
+        noteContent1.className = 'result';
+        noteContent1.innerHTML = `
+            <div class="result-title">
+                <a href="http://${allDataKeys[i]}">${edjsData['TITLE']}</a>
+            </div>
+            <div class="result-url">http://${allDataKeys[i]}</div>
+            <div class="result-summary">${edjsData['TAGS']}</div>
+            <button class="toggle-notes" onclick="toggleNotes(${i})">Show Notes</button>
+        `;
+    
+        if (foo_loaded_keys.length > 0) {
             // Only insert div to page if there are notes
             document.getElementById('note-container').appendChild(noteContent1);
-            document.getElementById('note-container').appendChild(noteContent);
         }
-
-
-        for(let j=0;j<foo_loaded_keys.length;j++){
-
-
+    
+        for (let j = 0; j < foo_loaded_keys.length; j++) {
             var noteDiv = document.createElement('div');
             noteDiv.className = 'note';
-            noteDiv.style.backgroundColor = colorChosen;
             noteDiv.id = 'note' + note_count;
-
+    
             var titleInput = document.createElement('div');
             titleInput.className = 'note-title';
-            var titleCount = j+1;
-            var tagsDiv = '';
-           
-            var counter = j+1;
-
-            titleInput.innerHTML = `Note-${counter} <a href='http://${allDataKeys[i]}#tooltip${titleCount}'>[Edit]</a> <a href='http://0.0.0.0:3000/notesViewer?q=${allDataKeys[i]}#tooltip${titleCount}'>[Cite]</a><br>${tagsDiv}<br>`;
-
+            var titleCount = j + 1;
+    
+            titleInput.innerHTML = `
+                Note-${titleCount} 
+                <a href="http://${allDataKeys[i]}#tooltip${titleCount}">[Edit]</a> 
+                <a href="http://0.0.0.0:3000/notesViewer?q=${allDataKeys[i]}#tooltip${titleCount}">[Cite]</a>
+            `;
+    
             noteDiv.appendChild(titleInput);
-
-
-            document.getElementById('note-content' + i).appendChild(noteDiv);
-
+            noteContentWrapper.appendChild(noteDiv);
+    
             var edjsnote = new EditorJS({
-                holder: "note"+note_count,
+                holder: "note" + note_count,
                 tools: {
-                  header: {
-                    class: Header,
-                    inlineToolbar:true,
-                    config: {
-                    placeholder: 'Header'
+                    header: {
+                        class: Header,
+                        inlineToolbar: true,
+                        config: {
+                            placeholder: 'Header'
+                        },
+                        shortcut: 'CMD+SHIFT+H'
                     },
-                    shortcut: 'CMD+SHIFT+H'
-                  },
-                  image: SimpleImage,
-                  embed: {
-                    class: Embed,
-                    config: {
-                      services: {
-                        youtube: true,
-                        github: true,
-                        vimeo: true,
-                      }
-                    }
-                  },
-                  list: {
-                    class: List,
-                    inlineToolbar: true,
-                    shortcut: 'CMD+SHIFT+L'
-                  },
-                  quote: {
-                    class: Quote,
-                    inlineToolbar: true,
-                    config: {
-                    quotePlaceholder: 'Enter a quote',
-                    captionPlaceholder: 'Quote\'s author',
+                    image: SimpleImage,
+                    embed: {
+                        class: Embed,
+                        config: {
+                            services: {
+                                youtube: true,
+                                github: true,
+                                vimeo: true,
+                            }
+                        }
                     },
-                    shortcut: 'CMD+SHIFT+O'
-                  },
-                  code: CodeTool,
-                  },
-                data:  edjsData['JSON'][foo_loaded_keys[j]],
+                    list: {
+                        class: List,
+                        inlineToolbar: true,
+                        shortcut: 'CMD+SHIFT+L'
+                    },
+                    quote: {
+                        class: Quote,
+                        inlineToolbar: true,
+                        config: {
+                            quotePlaceholder: 'Enter a quote',
+                            captionPlaceholder: 'Quote\'s author',
+                        },
+                        shortcut: 'CMD+SHIFT+O'
+                    },
+                    code: CodeTool,
+                },
+                data: edjsData['JSON'][foo_loaded_keys[j]],
                 readOnly: true
-
-              });
-                note_count+=1;
-
+            });
+            note_count += 1;
         }
+    
+        document.getElementById('note-container').appendChild(noteContentWrapper);
     }
 }
 
+function toggleNotes(index) {
+    var notes = document.querySelectorAll('.note-content')[index].querySelectorAll('.note');
+    notes.forEach(note => {
+        if (note.style.display === 'none' || note.style.display === '') {
+            note.style.display = 'block';
+        } else {
+            note.style.display = 'none';
+        }
+    });
+}
 
 // Search functionality/////
 function searchDB(){
